a072d28090fa818b6895a16d532cc3d83bc1383c
Rafael Lunardelli <rafael@devsquad.com>
2018-06-28 14:06:28 -0600

    authentication

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,18 @@ if (!defined('DEFAULT_VERSION')) {
     define('DEFAULT_VERSION', '1.0');
 }
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    Route::resource('docs', 'DocumentationsController');
+});
+
 Route::get('/', function () {
     return redirect('/introduction');
 });
 
 Route::get('/{version}/{page?}', 'DocumentationController@show');
+
+
